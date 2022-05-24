b6669e026847a352e2fa9cc83e08f64fe936d532
andre cantarino <cantarino.andre@gmail.com>
2022-05-24 18:17:24 -0300

    added popover tests

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -3,12 +3,22 @@ import Form from "react-bootstrap/Form";
 import FormCheck from "react-bootstrap/FormCheck";
 import FormGroup from "react-bootstrap/FormGroup";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 const SummaryForm = () => {
   const [tcChecked, setTcChecked] = useState(false);
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}>Terms and conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
