81f5ece1d6c2ca953a1311140d40ce5c17fccc37
Andre Cantarino <cantarino.andre@gmail.com>
2022-05-26 14:40:06 -0300

    [Section 05] Simulating server response with mock service worker (#2)
    * mock server setup
    
    * fix Options test when we search images
    
    * added topping component and test
    
    * fix OrderEntry test addind waitFor
    
    * added explicit return to OrderEntry test handler

---
+++ src/pages/entry/test/Options.test.jsx
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import { element } from "prop-types";
+import Options from "../Options";
+
+test("display image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  //find images
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+  expect(scoopImages).toHaveLength(2);
+
+  //confirm ALT text of images
+  const altText = scoopImages.map((element) => element.alt);
+  expect(altText).toEqual(["Chocolate scoop", "Vanilla scoop"]);
+});
+
+test("display image for each toppings option from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  //find images
+  const toppingsImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+  expect(toppingsImages).toHaveLength(3);
+
+  //confirm ALT text of images
+  const altText = toppingsImages.map((element) => element.alt);
+  expect(altText).toEqual([
+    "Cherries topping",
+    "M&Ms topping",
+    "Hot fudge topping",
+  ]);
+});
